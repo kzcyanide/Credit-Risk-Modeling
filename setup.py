073e2a61a38da53545bf484e7a22d8b7a43fd283
kzcyanide <snjt700@gmail.com>
2024-04-24 17:23:30 +0530

    init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="Credit-Risk-Modeling",
+    version="0.0.1",
+    author="kzcyanide",
+    author_email="snjt700@gmail.com",
+    packages=find_packages()
+
+)
